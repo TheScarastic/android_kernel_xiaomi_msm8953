b11adbca688080a851f2e04284b17b78589dc39f
TheScarastic <warabhishek@gmail.com>
2018-03-21 14:51:57 +0530

    driver: media: platform: msm: Import tissot camera_v2 changes

--- drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
+++ drivers/media/platform/msm/camera_v2/sensor/eeprom/msm_eeprom.c
@@ -151,6 +151,7 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 	struct msm_eeprom_memory_map_t *emap = block->map;
 	struct msm_eeprom_board_info *eb_info;
 	uint8_t *memptr = block->mapdata;
+	uint8_t sensor_id[2] = {0};
 
 	if (!e_ctrl) {
 		pr_err("%s e_ctrl is NULL", __func__);
@@ -158,6 +159,15 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 	}
 
 	eb_info = e_ctrl->eboard_info;
+
+	e_ctrl->i2c_client.addr_type = 2;
+	rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_read_seq(
+			&(e_ctrl->i2c_client), 0x0000,
+			sensor_id, 2);
+	if (rc < 0) {
+		pr_err("%s %d error\n", __func__, __LINE__);
+		return rc;
+	}
 
 	for (j = 0; j < block->num_map; j++) {
 		if (emap[j].saddr.addr) {
@@ -173,7 +183,13 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 			rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_write(
 				&(e_ctrl->i2c_client), emap[j].page.addr,
 				emap[j].page.data, emap[j].page.data_t);
-				msleep(emap[j].page.delay);
+
+			if (emap[j].page.delay > 20)
+			    msleep(emap[j].page.delay);
+			else if (emap[j].page.delay)
+			    usleep_range(emap[j].page.delay * 1000,
+				(emap[j].page.delay * 1000) + 1000);
+
 			if (rc < 0) {
 				pr_err("%s: page write failed\n", __func__);
 				return rc;
@@ -184,7 +200,13 @@ static int read_eeprom_memory(struct msm_eeprom_ctrl_t *e_ctrl,
 			rc = e_ctrl->i2c_client.i2c_func_tbl->i2c_write(
 				&(e_ctrl->i2c_client), emap[j].pageen.addr,
 				emap[j].pageen.data, emap[j].pageen.data_t);
+
+			if (emap[j].pageen.delay > 20)
 				msleep(emap[j].pageen.delay);
+			else if (emap[j].pageen.delay)
+				usleep_range(emap[j].pageen.delay * 1000,
+						(emap[j].pageen.delay * 1000) + 1000);
+
 			if (rc < 0) {
 				pr_err("%s: page enable failed\n", __func__);
 				return rc;
@@ -365,7 +387,13 @@ static int eeprom_parse_memory_map(struct msm_eeprom_ctrl_t *e_ctrl,
 					eeprom_map->mem_settings[i].reg_addr,
 					eeprom_map->mem_settings[i].reg_data,
 					eeprom_map->mem_settings[i].data_type);
-				msleep(eeprom_map->mem_settings[i].delay);
+
+				if (eeprom_map->mem_settings[i].delay > 20)
+					msleep(eeprom_map->mem_settings[i].delay);
+				else if (eeprom_map->mem_settings[i].delay)
+					usleep_range(eeprom_map->mem_settings[i].delay * 1000,
+							(eeprom_map->mem_settings[i].delay * 1000) + 1000);
+
 				if (rc < 0) {
 					pr_err("%s: page write failed\n",
 						__func__);
@@ -397,7 +425,13 @@ static int eeprom_parse_memory_map(struct msm_eeprom_ctrl_t *e_ctrl,
 					eeprom_map->mem_settings[i].reg_addr,
 					memptr,
 					eeprom_map->mem_settings[i].reg_data);
-				msleep(eeprom_map->mem_settings[i].delay);
+
+				if (eeprom_map->mem_settings[i].delay > 20)
+					msleep(eeprom_map->mem_settings[i].delay);
+				else if (eeprom_map->mem_settings[i].delay)
+					usleep_range(eeprom_map->mem_settings[i].delay * 1000,
+							(eeprom_map->mem_settings[i].delay * 1000) + 1000);
+
 				if (rc < 0) {
 					pr_err("%s: read failed\n",
 						__func__);
